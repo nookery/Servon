c45c1a963db7b0749a295125cb638100e3658b54
Nookery <nooks@qq.com>
2025-02-01 22:53:42 +0800

    🎨 Chore:  Minor adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,39 +2,40 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		// @ts-ignore
-		plugins: [tailwindcss()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:8080',
-				changeOrigin: true,
-			},
-		},
-	},
-	},
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'API',
-					items: [
-						{ label: '基本信息', slug: 'api/basic' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	]
+    vite: {
+        // @ts-ignore
+        plugins: [tailwindcss()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+        },
+    },
+    },
+    integrations: [
+        starlight({
+        title: 'My Docs',
+        social: {
+            github: 'https://github.com/withastro/starlight',
+        },
+        sidebar: [
+            {
+                label: 'Web API',
+                items: [
+                    { label: '基本信息', slug: 'web_api/basic' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+    }),
+        vue()]
 });
